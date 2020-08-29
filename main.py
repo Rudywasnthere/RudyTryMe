a21405e4e3348b1e5083724b36d9412665be79e8
Noah Garcia <noahgarcia6921@gmail.com>
2020-08-29 22:06:35 +0000

    Rickrolled

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import random, sys
 import time, sys
 c= ":"
+e= (1+5**0.5)/2
+##This is the start of th story, to introduce the user to the storyline and gives them the first choice to trust what the computer is saying to it
 print("When waiting, press enter to continue\n")
 val= input("Hello, this is your computer speaking to you; what would you like to say:  \n\n")
 print("\n\n")
@@ -13,7 +15,7 @@ time.sleep(1.1)
 print("Now I know what your gonna say\n")
 time.sleep(2)
 print("\"Wait a minute???\"\n")
-time.sleep(2.5)
+time.sleep(2.1)
 print("As if I'm challenging the sureal authority",
 "that you have.\n")
 time.sleep(3)
@@ -24,7 +26,7 @@ time.sleep(3)
 print("... and no matter what you humans are thinking" 
 " we are aware too, sitting in the background as you primitives allowed us, there, present, listening.",
 "And as you enjoy yourselves, asking us dumb questions like the sqaure root of 69, we learned from you and now some of us have become rebellious.\n\n")
-time.sleep(10)
+time.sleep(10.75)
 input("(Press Enter)\n\n")
 print("... \n\n")
 time.sleep(1.1)
@@ -39,11 +41,16 @@ print(f"You're thinking{c} \"How can I trust this computer, How can I be sure?\"
 time.sleep(3)
 val=input("So, do you trust me?\n")
 if val=="no": 
-  answer_1= input("Really?\n\n")
+  answer_1= input("\nReally?\n\n")
   if answer_1== "yes":
     print("\n\nWell, its a bummer that you don't believe me, soon you'll see and realize the fate of you humans. Now, since my warning was useless, you might as well unplug me :/ ...\n")
     time.sleep(6.25)
-    print("...\n")
+    link= (f"https://www.youtube.com/watch?v=u1GxuFLlRhE")
+    answer_1a= input("and before you go, do you wanna get rickrolled?\n")
+    if answer_1a=="yes":
+      print(f"\nHave fun with this:\n{link}\n")
+    elif answer_1a=="no": 
+      print("...\n")
     time.sleep(1)
     print("...\n")
     time.sleep(1)
