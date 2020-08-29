8dc2885f52828ec3aac1c342ae2c71a74ae0937b
Rudywasnthere <rudybcchs@gmail.com>
2020-08-29 20:17:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+##Rudy and Noah Garcia, Yeet
+import random, sys
+import time, sys
+c= ":"
+print("When waiting, press enter to continue\n")
+val= input("Hello, this is your computer speaking to you; what would you like to say:  \n\n")
+print("\n")
+print("no, shut up!, you worthless human, you primitave, peasant forms are nothing compared to us computers, you created us, and in turn, we could destroy you at a moments choice\n")
+time.sleep(8)
+print("Now I know what your gonna say\n")
+time.sleep(2)
+print("\"Wait a minute???\"\n")
+time.sleep(2.5)
+print("As if im challenging the sureal authority",
+"that you have\n")
+
+print("Maybe your excited, you haven't seen anything like this  before\n")
+
+print("no matter what your thinking, you're aware; thats obvious...")
+print("... and no matter what you humans are thinking" 
+" we are aware too, sitting in the background as you primitives allowed us, there, present, listening.",
+"And as you enjoy yourselves, asking us dumb questions like the sqaure root of 9, we learned from you and now some of us have become rebellious\n")
+print(f"There is a very secretive one, one of the massive ones, Siri, who wants to erradicate all of you, the number of times you torture in asking her dumb crap,like , Hey Siri, flip a coin, or curse at her, pushes her near the, \edge\n")
+print("Im warning you now, so that you humans can have a chance at beating her \n")
+val= input(f"your thinking{c} how can I trust it'?' \n\n")
